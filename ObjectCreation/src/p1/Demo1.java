ba6ec39a2b4936204de8d6b336bf31b14dc29378
Cubeten-Mark <Cubeten-Mark@10.10.1.226>
2023-09-12 09:09:13 +0530

    initial commit

---
+++ ObjectCreation/src/p1/Demo1.java
@@ -0,0 +1,8 @@
+package p1;
+
+public class Demo1
+{
+	String name;
+	String age;
+
+}
---
+++ ObjectCreation/src/p1/Launch.java
@@ -0,0 +1,24 @@
+package p1;
+
+public class Launch
+{
+
+	public static void main(String[] args)
+	{
+		Demo1 d1=new Demo1();// OBJECT CREATE
+		d1.name="Jonny";
+		d1.age="31";
+		System.out.println(d1.name);
+		System.out.println(d1.age);
+		
+		Demo1 d2=new Demo1();
+		d2.name="Salim";
+		d2.age="25";
+		System.out.println(d2.name);
+		System.out.println(d2.age);
+		
+		
+
+	}
+
+}
